0af32da98cbd960352e885ceee0f4463e8b73b99
Benoit Pierre <benoit.pierre@gmail.com>
2017-09-17 04:02:21 +0200

    add support for machine configuration through the Qt GUI

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+__requires__ = '''
+plover>=4.0.0.dev3
+setuptools>=30.3.0
+'''
+
 from setuptools import setup
 
-setup()
+from plover_build_utils.setup import BuildPy, BuildUi
+
+
+BuildPy.build_dependencies.append('build_ui')
+BuildUi.hooks = ['plover_build_utils.pyqt:gettext']
+cmdclass = {
+    'build_py': BuildPy,
+    'build_ui': BuildUi,
+}
+
+setup(cmdclass=cmdclass)
