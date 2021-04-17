599c7626cd98a90180d26565bb653ff8d79ed3d7
Benoit Pierre <benoit.pierre@gmail.com>
2021-04-17 20:29:58 +0200

    setup: ensure the i18n hook is disabled
    Older development versions of Plover enable it per-default.

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from plover_build_utils.setup import BuildPy, BuildUi
 
 
 BuildPy.build_dependencies.append('build_ui')
+if 'plover_build_utils.pyqt:gettext' in BuildUi.hooks:
+    BuildUi.hooks.remove('plover_build_utils.pyqt:gettext')
 cmdclass = {
     'build_py': BuildPy,
     'build_ui': BuildUi,
